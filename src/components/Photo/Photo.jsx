cc5982586467cb20b5f272dd4c1b4cc164003dcb
Axel Olsson <olsson.axel@gmail.com>
2020-07-08 08:48:01 +0200

    build(style): add CI with linting for components and styles

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Photo = ({ photos }) => {
-  return (
-    <div>
-      {photos && photos.map((photo) => {
-        return (
-          <div className="FreshBrews-box" key={photo.title}>
-            <a href={photo.src}>
-              <img src={photo.src} alt="" />
-            </a>
-            <h3>{photo.title}</h3>
-            <p>{photo.category}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+const Photo = ({ photos }) => (
+  <div>
+    {photos && photos.map((photo) => (
+      <div className="FreshBrews-box" key={photo.title}>
+        <a href={photo.src}>
+          <img src={photo.src} alt="" />
+        </a>
+        <h3>{photo.title}</h3>
+        <p>{photo.category}</p>
+      </div>
+    ))}
+  </div>
+);
 
 Photo.propTypes = {
-  photos: PropTypes.array.isRequired
+  photos: PropTypes.oneOfType([PropTypes.object]).isRequired,
 };
 
 export default Photo;
